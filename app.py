2a27232926125532b03d4822b0030d97baac07c5
Zain Gill <zaingill45@gmail.com>
2025-07-11 11:21:34 -0600

    update

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(800, 600)
+        self.setWindowTitle("Markdown Editor")
+
+        self.button = QPushButton("Click Me")
+
+        self.button.clicked.connect(self.button_clicked)
+        self.button.clicked.connect(self.button_toggled)
+
+        self.setCentralWidget(self.button)
+
+    def button_clicked(self):
+        self.button.setText("You already clicked me.")
+        self.button.setEnabled(False)
+        self.setWindowTitle("A new window title")
+
+    def button_toggled(self, checked):
+        print("Check State:", checked)
+
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()
